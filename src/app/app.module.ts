7abbe867ef45abaa3b3f72056b3f6941c108da5e
Remya <remyaj@qburst.com>
2017-02-13 10:24:43 +0530

    adding table

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-
 import { AppComponent } from './app.component';
-
+import { LoginModule } from './login/login.module';
+import { RegisterModule } from './register/register.module';
+import { AppRoutingModule } from './app-routing.module';
+// import {SharedModule} from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,11 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    LoginModule,
+    AppRoutingModule,
+    RegisterModule
+    // SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
